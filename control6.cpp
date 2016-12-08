126591843052a7f5a985f239fc1d03b63f76e7fb
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 10:17:03 +0330

    baresi adad az nazar + ya - bodan

---
+++ control6.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "cmath"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+int integer;
+cout<<"enter an integer :";
+cin>>integer;
+if (integer>0) cout<<"positive\n ";
+else if (integer==0)  cout<<"zero\n";
+else  cout<<"negative\n ";
+  return 0;
+}
