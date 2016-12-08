4773bb6b450c31e0f867d7f5b5ab961963592ba0
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 10:08:49 +0330

    baresi harf az nazare seda dari

---
+++ control2.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+char character;
+ cout<<"enter a character : ";
+cin>> character;
+if (character=='a'||character=='e'||character=='i'||character=='o'||character=='u')  cout<<"vowel\n";
+else cout<<"not vowel\n";
+  return 0;
+}
